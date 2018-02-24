6b739a3a80d759f0ab3e671f02f8968bc9330408
Fumio SAGAWA <fumio.sagawa@gmail.com>
2018-02-24 17:05:30 +0900

    add issue

--- src/app/pages/issue/issue.component.ts
+++ src/app/pages/issue/issue.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Issue } from './issue';
 
 @Component({
   selector: 'ah-issue',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssueComponent implements OnInit {
 
-  constructor() { }
+  issue: Issue;
+  issues: Issue[];
 
-  ngOnInit() {
+  constructor () {}
+
+  ngOnInit(): void {
+    this.issue = new Issue;
+    this.issues= [];
+  }
+
+  public onSubmit(form: NgForm) {
+    const issue = {
+      title: form.value.title,
+      desc: form.value.desc
+    }
+
+    this.issues.push(issue);
+
+    form.reset();
+  }
+
+  public onDelete(index: number) {
+    this.issues.splice(index, 1);
   }
 
 }
